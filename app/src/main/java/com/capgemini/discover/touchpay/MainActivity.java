2f80c1f958574b9ba57aa1f161666a3899fe3caa
Akshay Jain <akshayjn1992@gmail.com>
2017-10-01 17:54:33 +0530

    Updated card selection criteria

--- app/src/main/java/com/capgemini/discover/touchpay/MainActivity.java
+++ app/src/main/java/com/capgemini/discover/touchpay/MainActivity.java
@@ -6,39 +6,108 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button getCardButton;
-    private TextView cardTexttView;
+    public static final String GREEN = "green";
+    public static final String PURPLE = "purple";
+    public static final String SILVER = "silver";
+
+    public static final String INTENT_EXTENDED_DATA = "MainActivity.cardType";
+
+    //    Button getCardButton;
+//    private TextView cardTexttView;
+    ImageView greenCard;
+    ImageView purpleCard;
+    ImageView silverCard;
+
+    CardDetails cardDetails;
+    CardDetails updatedCardDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getCardButton = (Button) findViewById(R.id.button);
-        cardTexttView = (TextView) findViewById(R.id.textView);
+        greenCard = (ImageView)findViewById(R.id.cardGreen);
+        purpleCard = (ImageView)findViewById(R.id.cardPurple);
+        silverCard = (ImageView)findViewById(R.id.cardSilver);
 
-        getCardButton.setOnClickListener(new View.OnClickListener(){
+        greenCard.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-//                cardTexttView.setText(getCardDetails().toString());
-//                cardTexttView.setTextColor(Color.BLACK);
-                Intent intent = new Intent(MainActivity.this,WebViewActivity.class);
+
+                updatedCardDetails = getCardDetails(GREEN);
+
+                Intent intent = new Intent(MainActivity.this,WebViewActivity.class).putExtra(INTENT_EXTENDED_DATA, updatedCardDetails);
                 startActivity(intent);
             }
         });
+
+        purpleCard.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                updatedCardDetails = getCardDetails(PURPLE);
+
+                Intent intent = new Intent(MainActivity.this,WebViewActivity.class).putExtra(INTENT_EXTENDED_DATA, updatedCardDetails);
+                startActivity(intent);
+            }
+        });
+
+        silverCard.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                updatedCardDetails = getCardDetails(SILVER);
+
+                Intent intent = new Intent(MainActivity.this,WebViewActivity.class).putExtra(INTENT_EXTENDED_DATA, updatedCardDetails);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+//        getCardButton = (Button) findViewById(R.id.button);
+//        cardTexttView = (TextView) findViewById(R.id.textView);
+//
+//        getCardButton.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                cardTexttView.setText(getCardDetails().toString());
+//               cardTexttView.setTextColor(Color.BLACK);
+//                Intent intent = new Intent(MainActivity.this,WebViewActivity.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 
-//    private CardDetails getCardDetails(){
-//        CardDetails cardDetails = new CardDetails();
-//        cardDetails.setCardNumber("4587 7854 2654 8568");
-//        cardDetails.setExpiryDate("06/22");
-//        cardDetails.setNameOnCard("Akshay Jain");
-//
-//        return cardDetails;
-//    }
+    private CardDetails getCardDetails(String card){
+
+        cardDetails = new CardDetails();
+
+        if(GREEN.equals(card)){
+            cardDetails.setCardNumber("6011 7854 2654 1111");
+            cardDetails.setExpiryDate("06/21");
+            cardDetails.setNameOnCard("Shailesh Bhave");
+        }
+        else if(PURPLE.equals(card)){
+            cardDetails.setCardNumber("6011 7854 2654 2222");
+            cardDetails.setExpiryDate("07/21");
+            cardDetails.setNameOnCard("Akshay Jain");
+        }
+        else if(SILVER.equals(card)){
+            cardDetails.setCardNumber("6011 7854 2654 3333");
+            cardDetails.setExpiryDate("08/21");
+            cardDetails.setNameOnCard("Tom Cruise");
+        }
+
+        return cardDetails;
+    }
 }
--- app/src/main/java/com/capgemini/discover/touchpay/WebViewActivity.java
+++ app/src/main/java/com/capgemini/discover/touchpay/WebViewActivity.java
@@ -1,6 +1,7 @@
 package com.capgemini.discover.touchpay;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,18 +12,18 @@ import android.webkit.WebViewClient;
 
 public class WebViewActivity extends Activity {
 
-    String url = "http://sites.simbla.com/35ef1b11-33e3-a875-7887-509b61c410c4/paymentgateway";
+    public static final String INTENT_EXTENDED_DATA = "MainActivity.cardType";
 
-    CardDetails cardDetails = new CardDetails();
+    public static final String url = "http://sites.simbla.com/35ef1b11-33e3-a875-7887-509b61c410c4/paymentgateway";
+
+    CardDetails cardDetails;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        cardDetails.setCardNumber("4587 7854 2654 8568");
-        cardDetails.setExpiryDate("06/22");
-        cardDetails.setNameOnCard("Akshay Jain");
+        cardDetails = (CardDetails)getIntent().getSerializableExtra(INTENT_EXTENDED_DATA);
 
         setContentView(R.layout.activity_web_view);
 
@@ -36,7 +37,7 @@ public class WebViewActivity extends Activity {
                 final String js = "javascript:" +
                         "document.getElementById('P129').value = '" + cardDetails.getCardNumber() + "';"  +
                         "document.getElementById('P132').value = '" + cardDetails.getExpiryDate() + "';" +
-                        "document.getElementById('P135').value = '" + cardDetails.getExpiryDate() + "';";
+                        "document.getElementById('P135').value = '" + cardDetails.getNameOnCard() + "';";
 
                 if (Build.VERSION.SDK_INT >= 19) {
                     view.evaluateJavascript(js, new ValueCallback<String>() {
